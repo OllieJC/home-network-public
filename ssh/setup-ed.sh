54dcc346fc597df9142c982f6f57ce40eea620e3
OllieJC <github@olliejc.uk>
2022-01-16 13:55:26 +0000

    Setup scripts

---
+++ ssh/setup-ed.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl https://raw.githubusercontent.com/OllieJC/home-network-public/main/ssh/home-network-infra-ed25519.pub >> ~/.ssh/authorized_keys
+
+# curl -s -L https://raw.githubusercontent.com/OllieJC/home-network-public/main/ssh/setup-ed.sh | bash
---
+++ ssh/setup-rsa.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl https://raw.githubusercontent.com/OllieJC/home-network-public/main/ssh/home-network-infra-rsa.pub >> ~/.ssh/authorized_keys
+
+# curl -s -L https://raw.githubusercontent.com/OllieJC/home-network-public/main/ssh/setup-rsa.sh | bash
