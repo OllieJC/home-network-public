e3d461ea0b28d4031feb1cee9f5fedd123cfa060
OllieJC <github@olliejc.uk>
2022-01-16 19:46:03 +0000

    Fix directory slashes

--- applications/admin-git.sh
+++ applications/admin-git.sh
@@ -4,7 +4,7 @@ CWD=$(pwd)
 
 if [ "$EUID" -eq 0 ]; then echo "Do not run as root" && exit 1; fi
 
-GPK="$HOME/.ssh/id_ed25519.github"
+GPK="${HOME}.ssh/id_ed25519.github"
 if [ -f "$GPK" ]; then
   echo "GitHub private key exists, continuing..."
   chmod 400 "$GPK"
@@ -13,7 +13,7 @@ else
   exit 1
 fi
 
-GHNP="$HOME/github/home-network-public"
+GHNP="${HOME}github/home-network-public"
 if [ -d "$GHNP" ]; then
   echo "GitHub repo 'home-network-public' exists, continuing..."
 else
@@ -21,7 +21,7 @@ else
   exit 1
 fi
 
-ssh-keyscan -t rsa github.com >> "$HOME/.ssh/known_hosts"
+ssh-keyscan -t rsa github.com >> "${HOME}.ssh/known_hosts"
 
 git config --global user.email "github@olliejc.uk"
 git config --global user.name "OllieJC"
@@ -30,7 +30,7 @@ echo """Host github.com
     PreferredAuthentications publickey
     Hostname github.com
     IdentityFile $GPK
-    IdentitiesOnly yes""" > "$HOME/.ssh/config"
+    IdentitiesOnly yes""" > "${HOME}.ssh/config"
 
 cd "$GHNP" || exit 1
 git remote set-url origin git@github.com:OllieJC/home-network-public.git
